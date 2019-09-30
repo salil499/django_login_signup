5875e8fd193443cc56f9e3f2b7723c75a79ee7ae
salil499 <55999182+salil499@users.noreply.github.com>
2019-10-01 00:15:05 +0530

    Update models.py

--- login/models.py
+++ login/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=1000)
-    password = models.CharField(max_length=500)
-    token = models.CharField(max_length=1000)
+    username  =  models.CharField(max_length=1000)
+    password  =  models.CharField(max_length=500)
+    token  =  models.CharField(max_length=1000)
